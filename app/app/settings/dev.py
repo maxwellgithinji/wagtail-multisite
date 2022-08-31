757ada142d1b9ec33eee4c8bcb72e70e07f23024
maxwellgithinji <maxwellgithinji@gmail.com>
2022-08-31 12:13:00 +0300

    feat: setup
    Signed-off-by: maxwellgithinji <maxwellgithinji@gmail.com>

---
+++ app/app/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-zk0o$!lv!=8wxny8ie^t%-^yw-msh=i-ksr=tl%k-gud()$f9!"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
